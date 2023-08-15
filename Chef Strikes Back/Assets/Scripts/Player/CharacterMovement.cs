04fe0131ba97c31bc664bfd689469f4a3411d828
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-08-15 15:41:40 -0700

    BetterMovement

--- Chef Strikes Back/Assets/Scripts/Player/CharacterMovement.cs
+++ Chef Strikes Back/Assets/Scripts/Player/CharacterMovement.cs
@@ -10,18 +10,30 @@ public class CharacterMovement : MonoBehaviour
     private float moveSpeed;
     [SerializeField]
     private Rigidbody2D rb;
-    private Vector2 moveDirection;
     [SerializeField]
     private float acceleration;
 
-    public Animator animator;
-
     private InputControls inputManager;
     private InputAction move;
+    private Vector2 moveDirection;
+    private Animator animator;
+    private int direction;
+
+    string[] directionNames =
+        {
+        "Idle_Right", "Idle_RightTop", "Idle_Front", "Idle_LeftTop",
+        "Idle_Left", "Idle_LeftBot", "Idle_Bot", "Idle_RightBot"
+    };
+    string[] attackDirection =
+        {
+        "Attack_Right", "Attack_RightTop", "Attack_Top", "Attack_LeftTop",
+        "Attack_Left", "Attack_LeftBot", "Attack_Bot", "Attack_RightBot"
+    };
 
     private void Awake()
     {
         inputManager = new InputControls();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -52,27 +64,21 @@ public class CharacterMovement : MonoBehaviour
         if (moveDirection != Vector2.zero)
         {
             FaceMovementDirection(moveDirection);
-        }       
+        }
     }
 
     public void FaceMovementDirection(Vector2 lookDirection)
     {
         //8 direction animaiton
         int directionIndex = Mathf.FloorToInt((Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 360 + 22.5f) / 45f) % 8;
-        string[] directionNames = { "Idle_Right", "Idle_RightTop", "Idle_Front", "Idle_LeftTop", "Idle_Left", "Idle_LeftBot", "Idle_Bot", "Idle_RightBot" };
         animator.Play(directionNames[directionIndex]);
-        Debug.Log(directionNames[directionIndex]);
+
+        if (direction != directionIndex) ChangeDirectionSpeed(directionIndex);
     }
     public string GetAttackDirection(Vector2 lookDirection)
     {
         int directionIndex = Mathf.FloorToInt((Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 360 + 22.5f) / 45f) % 8;
-        string[] directionNames =
-        {
-        "Attack_Right", "Attack_RightTop", "Attack_Top", "Attack_LeftTop",
-        "Attack_Left", "Attack_LeftBot", "Attack_Bot", "Attack_RightBot"
-    };
-
-        return directionNames[directionIndex];
+        return attackDirection[directionIndex];
     }
     public Vector2 GetMoveDirection()
     {
@@ -82,6 +88,12 @@ public class CharacterMovement : MonoBehaviour
     public void SpeedBoost(float boostAmount)
     {
         moveSpeed += boostAmount;
+    }
+
+    private void ChangeDirectionSpeed(int newDirection)
+    {
+        rb.velocity = moveDirection * rb.velocity.magnitude;
+        direction = newDirection;
     }
 }
 
