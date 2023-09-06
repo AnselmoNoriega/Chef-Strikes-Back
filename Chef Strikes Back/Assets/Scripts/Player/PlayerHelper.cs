837f19f7471a43ffad565100ccf35242ea954737
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-09-06 15:24:16 -0700

    Variables and classHelper

---
+++ Chef Strikes Back/Assets/Scripts/Player/PlayerHelper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHelper : MonoBehaviour
+{
+    public static void FaceMovementDirection(Animator animator, Vector2 lookDirection, string[] directionNames)
+    {
+        int directionIndex = Mathf.FloorToInt((Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 360 + 22.5f) / 45f) % 8;
+        animator.Play(directionNames[directionIndex]);
+    }
+
+    public string GetDirection(Vector2 lookDirection, string[] animDirection)
+    {
+        int directionIndex = Mathf.FloorToInt((Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 360 + 22.5f) / 45f) % 8;
+        return animDirection[directionIndex];
+    }
+}
