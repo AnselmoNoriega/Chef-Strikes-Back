bc56a488af6c9318de38719933bcb4ecb75e1f47
DawsonInkster <dawsoninkster@gmail.com>
2023-10-08 14:55:37 -0700

    Increased Customer timer
    I increased customer timer but realized pierre destroyed the map

--- Chef Strikes Back/Assets/Scripts/AI/StateManage/GoodCustomerState.cs
+++ Chef Strikes Back/Assets/Scripts/AI/StateManage/GoodCustomerState.cs
@@ -16,7 +16,7 @@ public class GoodCustomerState : StateClass<AI>
     {
         IsEat = false;
         readyOrder = false;
-        waitTime = 15.0f;
+        waitTime = 40.0f;
 
         if (TileManager.Instance.checkChairCount() > 0 && !agent.isSit)
         {
