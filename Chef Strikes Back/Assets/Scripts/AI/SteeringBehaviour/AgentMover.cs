679976895c1c1ed4ab1f5e95185cc6c81c9ac599
Connnnn9 <connnnn196470@gmail.com>
2023-10-25 23:47:39 -0700

    animations update
    updated all AI's animations that using agentmover, except some of them using path finding so it wont trigger the animation yet. keep working

--- Chef Strikes Back/Assets/Scripts/AI/SteeringBehaviour/AgentMover.cs
+++ Chef Strikes Back/Assets/Scripts/AI/SteeringBehaviour/AgentMover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AgentMover : MonoBehaviour
 {
+    private Animator anim;
     private Rigidbody2D rb2d;
 
     [SerializeField]
@@ -19,6 +20,7 @@ public class AgentMover : MonoBehaviour
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -35,9 +37,41 @@ public class AgentMover : MonoBehaviour
         }
         currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
         rb2d.velocity = oldMovementInput * currentSpeed;
-
-
-
-        
+        UpdateAnimation(MovementInput);
     }
+
+    private void UpdateAnimation(Vector2 movement)
+    {
+        if (movement.magnitude > 0)
+        {
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+
+            if (angle > -22.5 && angle <= 22.5)
+                anim.Play("AI_Walk_E");
+            else if (angle > 22.5 && angle <= 67.5)
+                anim.Play("AI_Walk_NE");
+            else if (angle > 67.5 && angle <= 112.5)
+                anim.Play("AI_Walk_N");
+            else if (angle > 112.5 && angle <= 157.5)
+                anim.Play("AI_Walk_NW");
+            else if ((angle > 157.5 && angle <= 180) || (angle >= -180 && angle <= -157.5))
+                anim.Play("AI_Walk_W");
+            else if (angle > -157.5 && angle <= -112.5)
+                anim.Play("AI_Walk_SW");
+            else if (angle > -112.5 && angle <= -67.5)
+                anim.Play("AI_Walk_S");
+            else
+                anim.Play("AI_Walk_SE");
+        }
+        else
+        {
+            anim.Play("AI_Walk_N");
+        }
+    }
+    /*    public static int FaceMovementDirection(Animator animator, Vector2 lookDirection)
+    {
+        int directionIndex = Mathf.FloorToInt((Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg + 360 + 22.5f) / 45f) % 8;
+        animator.SetInteger("PosNum", directionIndex);
+        return directionIndex;
+    }*/
 }
