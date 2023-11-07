9f60341fead5704e158a659bc5375048bb4891e5
JorbenTheDeveloper <134649421+JorbenTheDeveloper@users.noreply.github.com>
2023-11-06 20:04:31 -0800

    Music Start

---
+++ Chef Strikes Back/Assets/Scripts/Sounds/SoundTest.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTest : MonoBehaviour
+{
+    public AudioSource Audiocontroller;
+    public AudioSource BackgroundMusic;
+    public AudioSource Footsteps;
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Audiocontroller.Play();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Audiocontroller.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Footsteps.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Footsteps.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Footsteps.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Footsteps.Play();
+        }
+    }
+
+    private void Start()
+    {
+        BackgroundMusic.Play();
+    }
+}
+ 
