c3598d530d1f28887919bc979a8e03d9137063a7
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-12-04 19:28:49 -0800

    Level timer view

--- Chef Strikes Back/Assets/Scripts/World/LevelTimer.cs
+++ Chef Strikes Back/Assets/Scripts/World/LevelTimer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
@@ -11,33 +12,44 @@ public class LevelTimer : MonoBehaviour
     private float lightStartValue;
 
     [SerializeField]
-    private float elapsTime;
+    private float elapsedTime;
     private float elapsTimeStart;
     private TimeSpan timePlaying;
 
     [SerializeField] SceneControl sceneControl;
 
+    [Serializable]
+    public struct PhaseDefinition
+    {
+        public int StartTime;
+        public int EndTime;
+        public int SpawnTime;
+    }
+    private PhaseDefinition _currentPhase;
+    [SerializeField] private List<PhaseDefinition> _phases = new();
+
     public void Initialize()
     {
-        timePlaying = TimeSpan.FromMinutes(elapsTime);
-        elapsTimeStart = elapsTime;
+        timePlaying = TimeSpan.FromMinutes(elapsedTime);
+        elapsTimeStart = elapsedTime;
         textTime.text = timePlaying.ToString("mm':'ss'.'ff");
         lightStartValue = worldLight.falloffIntensity;
+        _currentPhase = _phases[0];
     }
 
     // Update is called once per frame
     void Update()
     {
         SpawnTimeChangeBasedOnTimer();
-        if (elapsTime > 0.00f)
+        if (elapsedTime > 0.00f)
         {
-            elapsTime -= Time.deltaTime / 60;
+            elapsedTime -= Time.deltaTime / 60;
             worldLight.falloffIntensity += (lightStartValue / elapsTimeStart) * Time.deltaTime / 60;
 
-            timePlaying = TimeSpan.FromMinutes(elapsTime);
+            timePlaying = TimeSpan.FromMinutes(elapsedTime);
             textTime.text = timePlaying.ToString("mm':'ss'.'ff");
         }
-        else if (elapsTime < 0)
+        else if (elapsedTime < 0)
         {
             sceneControl.switchToWinScene();
         }
@@ -45,23 +57,25 @@ public class LevelTimer : MonoBehaviour
 
     private void SpawnTimeChangeBasedOnTimer()
     {
-        if (elapsTime <= 5f && elapsTime > 4.9f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(5);
-        else if (elapsTime <= 4.9f && elapsTime > 4.5f)
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(15);
-        else if (elapsTime <= 4.5f && elapsTime > 3f)
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(10);
-        else if (elapsTime <= 3.5f && elapsTime > 3f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(5);
-        else if (elapsTime <= 3f && elapsTime > 2.5f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(10);
-        else if (elapsTime <= 2.5f && elapsTime > 2f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(10);
-        else if (elapsTime <= 2f && elapsTime > 1.5f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(10);
-        else if (elapsTime <= 1.5 && elapsTime > 0.5f) 
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(5);
-        else if (elapsTime <= 0.5f && elapsTime > 0f)
-            ServiceLocator.Get<GameLoopManager>().ChangeSpawnTime(100000);
+        var loopManager = ServiceLocator.Get<GameLoopManager>();
+
+        if (elapsedTime <= 5f && elapsedTime > 4.9f) 
+            loopManager.ChangeSpawnTime(5);
+        else if (elapsedTime <= 4.9f && elapsedTime > 4.5f)
+            loopManager.ChangeSpawnTime(15);
+        else if (elapsedTime <= 4.5f && elapsedTime > 3f)
+            loopManager.ChangeSpawnTime(10);
+        else if (elapsedTime <= 3.5f && elapsedTime > 3f) 
+            loopManager.ChangeSpawnTime(5);
+        else if (elapsedTime <= 3f && elapsedTime > 2.5f) 
+            loopManager.ChangeSpawnTime(10);
+        else if (elapsedTime <= 2.5f && elapsedTime > 2f) 
+            loopManager.ChangeSpawnTime(10);
+        else if (elapsedTime <= 2f && elapsedTime > 1.5f) 
+            loopManager.ChangeSpawnTime(10);
+        else if (elapsedTime <= 1.5 && elapsedTime > 0.5f) 
+            loopManager.ChangeSpawnTime(5);
+        else if (elapsedTime <= 0.5f && elapsedTime > 0f)
+            loopManager.ChangeSpawnTime(int.MaxValue);
     }
 }
