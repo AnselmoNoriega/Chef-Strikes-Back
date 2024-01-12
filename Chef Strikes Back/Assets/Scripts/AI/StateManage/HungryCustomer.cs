84a65fcba0225e3d3f9c1fbe02c5d3417d320e9d
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-01-12 09:41:52 -0800

    Changes mechanic AI

--- Chef Strikes Back/Assets/Scripts/AI/StateManage/HungryCustomer.cs
+++ Chef Strikes Back/Assets/Scripts/AI/StateManage/HungryCustomer.cs
@@ -31,7 +31,6 @@ public class HungryCustomer : StateClass<AI>
 
         if (scale.x <= 0)
         {
-            agent.SelectedChair.FreeTableSpace();
             agent.ChangeState(AIState.Rage);
         }
     }
@@ -53,6 +52,11 @@ public class HungryCustomer : StateClass<AI>
 
     public void Exit(AI agent)
     {
+        if (scale.x <= 0)
+        {
+            agent.SelectedChair.FreeTableSpace();
+        }
+
         agent.Indicator.SetIndicator(false, (IndicatorImage)agent.ChoiceIndex);
         agent.OrderBubble[agent.ChoiceIndex].gameObject.SetActive(false);
 
