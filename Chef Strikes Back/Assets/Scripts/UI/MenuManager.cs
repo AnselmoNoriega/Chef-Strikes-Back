c9f70c231218b80cf6d07d940b240df2412e138f
KingsReddit <connnnn196470@gmail.com>
2024-02-26 20:15:11 -0800

    Marc is bad in programming

---
+++ Chef Strikes Back/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("First Seclected Options")]
+    [SerializeField] private GameObject _LevelSelectFirst;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
