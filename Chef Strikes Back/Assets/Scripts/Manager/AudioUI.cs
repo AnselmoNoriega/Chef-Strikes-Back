4002d85606d2b770afef41baee944bbaa51c3a98
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-11-11 15:34:17 -0800

    Adding text to setting menu for volume

--- Chef Strikes Back/Assets/Scripts/Manager/AudioUI.cs
+++ Chef Strikes Back/Assets/Scripts/Manager/AudioUI.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AudioUI : MonoBehaviour
 {
     [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private TextMeshProUGUI _volumeText;
 
     private void Start()
     {
         _volumeSlider.value = AudioListener.volume * 100;
+        _volumeText.text = $"Volume: {_volumeSlider.value}%";
     }
 
     public void ChangeVolume()
     {
         AudioListener.volume = _volumeSlider.value / 100f;
+        _volumeText.text = $"Volume: {_volumeSlider.value}%";
     }
 }
